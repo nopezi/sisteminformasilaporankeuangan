c1cf7920bcb25f2b2cc03f2b682817eebf373e10
nopezi saputra <snopezi@gmail.com>
2018-01-29 13:16:08 +0700

    update database

--- member/update_pengeluaran.php
+++ member/update_pengeluaran.php
@@ -3,12 +3,16 @@ include '../koneksi.php';
 $id_keluar=$_POST['id_keluar'];
 $id_proxy=$_POST['id_proxy'];
 $tanggal=$_POST['tanggal'];
+$bulan=$_POST['bulan'];
+$tahun=$_POST['tahun'];
 $nama_proxy=$_POST['nama_proxy'];
 $detail=$_POST['detail'];
 $jumlah=$_POST['jumlah'];
 
 
-
+if($tanggal == "" || $bulan == ""){
+	echo '<script language="javascript">alert("Isi Semua Data"); document.location="pemasukkan.php";</script>';
+}else{
 mysqli_query($koneksi, "update keluar set
 			id_proxy='$id_proxy', 
 			tanggal='$tanggal', 
@@ -16,6 +20,6 @@ mysqli_query($koneksi, "update keluar set
 			detail='$detail', 
 			jumlah='$jumlah'
 			where id_keluar='$id_keluar'");
-header("location:pengeluaran.php");
-
+header("location:finance.php");
+}
 ?>
